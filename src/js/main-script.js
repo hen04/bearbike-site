4e69089af3c2d4bf409c8b20cd7ce83dd03cfbc1
DESKTOP-NDUN9JV\Nadya <nadia_degtiareva@rambler.ru>
2022-07-24 17:11:28 +0300

    menu mobile

--- src/js/main-script.js
+++ src/js/main-script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+window.onload = function () {
+	let page = document.querySelector('body');
+	let menuBtn = document.querySelector('.js-menu-btn');
+
+	menuBtn.addEventListener('click', function () {
+		page.classList.toggle('menu-opened');
+	});
+
+	let smoothLinks = document.querySelectorAll('.header-menu a[href^="#"]');
+	for (let smoothLink of smoothLinks) {
+		smoothLink.addEventListener('click', function (e) {
+			e.preventDefault();
+			let id = smoothLink.getAttribute('href');
+
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+
+			page.classList.toggle('menu-opened');
+		});
+	}
+
+}
